aaf0bcd026ae6aefcab6e078761842f44648b4ab
gabe <94710155+G1A1B1E@users.noreply.github.com>
2022-03-08 14:42:17 -0500

    di

--- src/onclickbutt.js
+++ src/onclickbutt.js
@@ -1,8 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+function showDiv() {
+  document.getElementById('welcomeDiv').style.display = "block";
+}
