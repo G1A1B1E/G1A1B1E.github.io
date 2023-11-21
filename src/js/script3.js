aa863967ca8dffa7234c2b1fed17c09898e0618c
Gabe Macri <gabemacru@gmail.com>
2023-11-21 14:03:34 -0500

    p3 indev make page

--- src/js/script3.js
+++ src/js/script3.js
@@ -1,2 +1,26 @@
 console.log("working...")
 
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.footer-link');
+    const movementSpeed = 3;
+    
+    const hoverLink = (event) => {
+        let targetNode = event.target;
+
+        const { left, top, width, height } = targetNode.getBoundingClientRect();
+
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+        
+        let x = (centerX - event.pageX) / movementSpeed;
+        let y = (centerY - event.pageY) / movementSpeed;
+    
+        targetNode.style.setProperty('--x', `${x}px`);
+        targetNode.style.setProperty('--y', `${y}px`);
+    }
+
+    links.forEach(link => {
+        link.addEventListener('mousemove', hoverLink);
+    });
+});
+
